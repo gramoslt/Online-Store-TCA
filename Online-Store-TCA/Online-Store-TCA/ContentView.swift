97253b31f700139890620a92829aeec18c5f8a9c
Gerardo Ramos <gerx_37@hotmail.com>
2025-01-18 13:51:04 -0600

    Created CounterDomain and view

--- Online-Store-TCA/Online-Store-TCA/ContentView.swift
+++ Online-Store-TCA/Online-Store-TCA/ContentView.swift
@@ -6,19 +6,75 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
+
+struct CounterDomain {
+    struct State: Equatable {
+        var counter = 0
+    }
+
+    enum Action: Equatable {
+        case increaseCounter
+        case decreaseCounter
+    }
+
+    struct Environment {
+
+    }
+
+    static let reducer = AnyReducer<State, Action, Environment> { state, action, environment in
+        switch action {
+            case .increaseCounter:
+                state.counter += 1
+                return .none
+            case .decreaseCounter:
+                state.counter -= 1
+                return .none
+        }
+    }
+}
 
 struct ContentView: View {
+    let store: Store<CounterDomain.State, CounterDomain.Action>
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        WithViewStore(self.store) { viewStore in
+            HStack {
+                Button {
+                    viewStore.send(.decreaseCounter)
+                } label: {
+                    Text("-")
+                        .padding(10)
+                        .background(.blue)
+                        .foregroundStyle(.white)
+                        .cornerRadius(10)
+                }
+                .buttonStyle(.plain)
+
+                Text(viewStore.state.counter.description)
+                    .padding(5)
+
+                Button {
+                    viewStore.send(.increaseCounter)
+                } label: {
+                    Text("+")
+                        .padding(10)
+                        .background(.blue)
+                        .foregroundStyle(.white)
+                        .cornerRadius(10)
+                }
+                .buttonStyle(.plain)
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(
+        store: Store(
+            initialState: CounterDomain.State(),
+            reducer: CounterDomain.reducer,
+            environment: CounterDomain.Environment()
+        )
+    )
 }
--- Online-Store-TCA/Online-Store-TCA/Online_Store_TCAApp.swift
+++ Online-Store-TCA/Online-Store-TCA/Online_Store_TCAApp.swift
@@ -6,12 +6,17 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 @main
 struct Online_Store_TCAApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(store: Store(
+                initialState: CounterDomain.State(),
+                reducer: CounterDomain.reducer,
+                environment: CounterDomain.Environment()
+            ))
         }
     }
 }
