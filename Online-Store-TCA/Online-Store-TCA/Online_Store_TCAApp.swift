7b628c87b40b03948630cdb4b21ce20bb49bcbd3
Gerardo Ramos <gerx_37@hotmail.com>
2025-01-18 12:31:59 -0600

    Created Xcode Project

---
+++ Online-Store-TCA/Online-Store-TCA/Online_Store_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Online_Store_TCAApp.swift
+//  Online-Store-TCA
+//
+//  Created by Gerardo Ramos on 18/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Online_Store_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
