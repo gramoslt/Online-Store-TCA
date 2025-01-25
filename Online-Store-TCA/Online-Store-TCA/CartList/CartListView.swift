d5d6f63fa4522ad876ac05f2b656bcc8654fde10
Gerardo Ramos <gerx_37@hotmail.com>
2025-01-25 13:48:05 -0600

    Created Cart View sheet presenting logic

---
+++ Online-Store-TCA/Online-Store-TCA/CartList/CartListView.swift
@@ -0,0 +1,18 @@
+//
+//  CartListView.swift
+//  Online-Store-TCA
+//
+//  Created by Gerardo Ramos on 25/01/25.
+//
+
+import SwiftUI
+
+struct CartListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CartListView()
+}
