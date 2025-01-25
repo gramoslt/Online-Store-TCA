489b5fb9b067735a39aa5571e349cbf284272b99
Gerardo Ramos <gerx_37@hotmail.com>
2025-01-25 15:29:18 -0600

    created CartList view and domain git status

---
+++ Online-Store-TCA/Online-Store-TCA/CartList/CartListDomain.swift
@@ -0,0 +1,40 @@
+//
+//  CartListDomain.swift
+//  Online-Store-TCA
+//
+//  Created by Gerardo Ramos on 25/01/25.
+//
+
+import Foundation
+import ComposableArchitecture
+
+struct CartListDomain: Equatable {
+    struct State: Equatable {
+        var cartItems: IdentifiedArrayOf<CartItemDomain.State> = []
+    }
+
+    enum Action: Equatable {
+        case didPressCloseButton
+        case cartItem(id: CartItemDomain.State.ID, action: CartItemDomain.Action)
+    }
+
+    struct Environment {}
+
+    static let reducer = AnyReducer<
+        State, Action, Environment
+    >.combine(
+        CartItemDomain.reducer.forEach(
+            state: \.cartItems,
+            action: /Action.cartItem(id:action:),
+            environment: {_ in CartItemDomain.Environment()}
+        ),
+        .init { state, action, environment in
+            switch action {
+                case .didPressCloseButton:
+                    return .none
+                case .cartItem:
+                    return .none
+            }
+        }
+    )
+}
